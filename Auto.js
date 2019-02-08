829215ea60e2d2236cae6ff734d13a6aa7bae5ef
pakkofrank <jpalma@ucol.mx>
2019-02-08 15:34:39 -0600

    el costructor

--- Auto.js
+++ Auto.js
@@ -1,10 +1,38 @@
-class Auto {
-    constructor(marca, modelo, color, kilometraje){
-        this.marca = marca;
-        this.modelo = modelo;
-        this.color = color;
-        this.kilometraje = 0;
-        this.estado = "apagado";
+export default class Auto {
+    constructor(marca, modelo, color = "blanco", kilometraje = 0){
+        this._marca = marca;
+        this._modelo = modelo;
+        this._color = color;
+        this._kilometraje = kilometraje;
+        this._estado = "apagado";
+    }
+    get color(){
+        return this._color;
+    }
+     set color(color){
+         this._color = color.toUpperCase();
+     }
+
+    mostrarEstado(){
+        console.log(`El auto ${this._marca} ${this._modelo} del
+         año ${this._color} tiene un kilometraje de ${this._kilometraje} ${this._estado}`);
+    }
+    encender(){
+        this._estado = "encendido";
+        this.mostrarEstado();
+    }
+    apagar(){
+        this._estado = "apagar";
+        this.mostrarEstado();
+    }
+    avanzar(velocidad, tiempo){
+        if(this._estado === "apagado"){
+            console.log("no puede avanzar porque esta apagado")
+        }else{
+            let distancia = velocidad * tiempo;
+            this._kilometraje = this._kilometraje + distancia
+            this.mostrarEstado();
+        }
+    
     }
 }
-SVGPathSegLinetoHorizontalAbs
---
+++ main.js
@@ -0,0 +1,18 @@
+import Auto from "./Auto.js";
+
+let auto1 = new Auto("ford", 2010, "rojo", 100);
+let auto2 = new Auto("nissan", 2018, "blanco");
+let auto3 = new Auto("vw", 2010,);
+
+console.log(`color ${auto1._color}`);
+console.log(`color ${auto2._color}`);
+auto1.color = "amarillo";
+auto2.color = "azul";
+
+auto2.encender();
+auto3.encender();
+
+auto1.encender();
+auto1.avanzar(50,2);
+auto1.apagar();
+auto1.avanzar(50,1);
